a289e55e41e7815d7c8cdf843298dbc00669acbc
qiujunlin <1757591067@qq.com>
2020-11-02 13:39:09 +0800

    电脑1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("电脑");
         System.out.println("电脑");
+        System.out.println("电脑1");
+
     }
 }
